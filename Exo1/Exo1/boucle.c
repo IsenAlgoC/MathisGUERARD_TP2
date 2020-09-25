e751146730eca44e470fd9a6bef1372d11011ca9
Sykfeuille <Sykfeuille@DESKTOP-AT7LD32>
2020-09-25 09:29:25 +0200

    yee

--- Exo1/Exo1/boucle.c
+++ Exo1/Exo1/boucle.c
@@ -4,15 +4,37 @@
 void main() {
 
 	//somme des n premiers entiers positifs
-	int n = 100;
+	int n = 5;
 	int sum = 0;
 	int i;
 	// avec une boucle for
 	for (i = 2; i <= n; i++) {
 		sum = sum + i;
-		printf("%d  ", sum);
 	}
-
+	printf("Somme = %d \nN = %d", sum, n);
 	printf("\n\n");
 	system("pause");
+
+	// avec une boucle while
+	sum = 0;
+	i = 2;
+	while (i <= n) {
+		sum = sum + i;
+		i = i + 1;
+	}
+	printf("Somme = %d \nN = %d", sum, n);
+	printf("\n\n");
+	system("pause");
+
+	// avec une boucle do while
+	sum = 0;
+	i = 2;
+	do {
+		sum = sum + i;
+		i = i + 1;
+	} while (!(i > n));
+	printf("Somme = %d \nN = %d", sum, n);
+	printf("\n\n");
+	system("pause");
+
 }
